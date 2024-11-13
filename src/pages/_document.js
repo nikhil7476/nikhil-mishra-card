788fb9ed32039949e2ee16091cc83808cdd83e96
nikNextup <nikhil.nextupgrad01@gmail.com>
2024-11-13 19:39:27 +0530

    modified:   src/pages/_document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/nikhil-mishra.jpg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
       </Head>
       <body>
