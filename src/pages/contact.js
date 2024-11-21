42e5d12b665b16c981695d33ad01352320a59525
nikNextup <nikhil.nextupgrad01@gmail.com>
2024-11-21 22:56:50 +0530

    modified:   src/pages/contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -84,9 +84,9 @@ export default function Contact() {
                   src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d197.2005048548882!2d79.31150607212938!3d26.559420525515897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sin!4v1732205980791!5m2!1sen!2sin"
                   width="100%"
                   height="100%"
-                  allowfullscreen=""
+                  allowFullScreen=""
                   loading="lazy"
-                  referrerpolicy="no-referrer-when-downgrade">
+                  referrerPolicy="no-referrer-when-downgrade">
                 </iframe>
               </div>
             </Col>
